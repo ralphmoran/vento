eee668a001493bb0de79b421e0715577cf0ddec5
ralph.moran <ralphmoran2003@gmail.com>
2021-04-29 18:16:56 -0700

    Added bluepint classes for Source and Resource

---
+++ Resource.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Resource class is exclusively used to perform HTTP methods/verbs, see README.md.
+ */
+
+ abstract class Resource {
+
+ }
---
+++ Source.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Source class is exclusively use to define the type of data to work on.
+ * 
+ * DB (SQL: MySQL, MSSQL... NON-SQL: MongoDB... ), 
+ * Cloud base (BigQuery, AWS...), 
+ * File (JSON, XML, CSV), 
+ */
+
+ abstract class Source {
+	
+ }
